7ecdf0a8ac194f1a1ef12591d35d16b371ca2573
Gijs Straathof <gbstraathof@gmail.com>
2018-11-24 10:11:25 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
